7f122eddf0e4fa539be17e0a5de9822723aa65c1
Daniel Ciaglia <daniel.ciaglia@tier.app>
2019-10-07 09:12:00 +0200

    use correct security_group
    - (!) breaks connectivity of existing worker nodes until rotated with new launch configuration
    - cleaner access to default_iam_role_id

--- local.tf
+++ local.tf
@@ -11,9 +11,9 @@ locals {
   cluster_security_group_id = var.cluster_create_security_group ? aws_security_group.cluster[0].id : var.cluster_security_group_id
   cluster_iam_role_name     = var.manage_cluster_iam_resources ? aws_iam_role.cluster[0].name : var.cluster_iam_role_name
   cluster_iam_role_arn      = var.manage_cluster_iam_resources ? aws_iam_role.cluster[0].arn : data.aws_iam_role.custom_cluster_iam_role[0].arn
-  worker_security_group_id  = var.worker_create_security_group ? aws_security_group.workers[0].id : var.worker_security_group_id
+  worker_security_group_id  = var.worker_create_security_group ? aws_security_group.workers_mapped[0].id : var.worker_security_group_id
 
-  default_iam_role_id = concat(aws_iam_role.workers_mapped.*.id, [""])[0]
+  default_iam_role_id = aws_iam_role.workers_mapped[0].id
   kubeconfig_name     = var.kubeconfig_name == "" ? "eks_${var.cluster_name}" : var.kubeconfig_name
 
   worker_group_count                       = length(var.worker_groups)
